3088dca3d5eadfefeb182ad4a94a8dcf43daaa2f
Raquib Saabit <raqdeku@outlook.com>
2023-09-19 10:17:10 +0000

    feat: installed dependancies

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+require('dotenv').config();
+const express = require('express');
+const startServer = require('./server');
+
+(async () => {
+  const app = startServer({ express });
+  app.listen(3000, () => {
+    console.log('App is listening....');
+  });
+})();
---
+++ src/server.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+
+const startServer = ({ express }) => {
+  const app = express();
+
+  return app;
+};
+
+module.exports = startServer;
