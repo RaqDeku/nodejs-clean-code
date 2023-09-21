cb28c51def3237b2e60dda56ee33010c73b2de20
Raquib Saabit <raqdeku@outlook.com>
2023-09-21 13:06:21 +0000

    fix: modified waitlist-db

--- src/data-access/waitlist-db.js
+++ src/data-access/waitlist-db.js
@@ -2,7 +2,7 @@ function makeWaitlistDb({ connectDb }) {
   async function findByEmail({ email }) {
     const db = await connectDb();
     const member = await db.collection('waitlist').findOne({ email });
-    return member && member;
+    return member ? { email, new: false } : null;
   }
 
   async function insert({ ...waitlistInfo }) {
@@ -10,6 +10,7 @@ function makeWaitlistDb({ connectDb }) {
     const result = await db.collection('waitlist').insertOne({
       ...waitlistInfo,
     });
+    return result ? { ...waitlistInfo.emai, new: true } : null;
   }
 
   return Object.freeze({
