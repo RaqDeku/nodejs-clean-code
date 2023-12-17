0f1bde57cf470a92cba318bb0e5b369c159bf465
Raquib Saabit <raqdeku@outlook.com>
2023-12-17 10:15:49 +0000

    Update join-waitlist.js

--- src/modules/waitlist/core/use-case/join-waitlist.js
+++ src/modules/waitlist/core/use-case/join-waitlist.js
@@ -3,27 +3,27 @@ const createMember = require('../entity/index');
 function buildJoinWaitlist({ waitlistDb, messageDispatcher }) {
   async function join({ name, email }) {
     const member = createMember({ name, email });
-    // const exists = await waitlistDb.findByEmail({ email: member.getEmail() });
+    const exists = await waitlistDb.findByEmail({ email: member.getEmail() });
 
-    // if (exists) {
-    //   return {
-    //     body: `${exists.email} already is on Waitlist!`,
-    //     statusCode: 200,
-    //   };
-    // }
+    if (exists) {
+      return {
+        body: `${exists.email} already is on Waitlist!`,
+        statusCode: 200,
+      };
+    }
 
-    // await Promise.all([
-    // await waitlistDb.insert({
-    //   name: member.getName(),
-    //   email: member.getEmail(),
-    //   joinedOn: member.getDateJoined(),
-    // }),
+    await Promise.all([
+    await waitlistDb.insert({
+      name: member.getName(),
+      email: member.getEmail(),
+      joinedOn: member.getDateJoined(),
+    }),
 
     await messageDispatcher.sendMessage({
       topic: 'waitlist_joined',
       message: { email: member.getEmail(), name: member.getName() },
     });
-    // ]);
+    ]);
 
     return {
       body: 'Successfully joined waitlist!',
